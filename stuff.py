3e2199e13a7b28457e7da6d0514625fee8a44a67
Wang Ziyuan <skyler24sg@gmail.com>
2026-07-24 17:32:53 +0800

    Update stuff.py

--- stuff.py
+++ stuff.py
@@ -1,1 +1,18 @@
-
+def quicksort(lst):
+    if not lst:
+        return []
+    equal_lst = []
+    equal = lst[len(lst)//2]
+    less_than = []
+    greater_equal = []
+    for num in lst:
+        if num < equal:
+            less_than.append(num)
+        elif num > equal:
+            greater_equal.append(num)
+        else:
+            equal_lst.append(num)
+    result = quicksort(less_than) + equal_lst + quicksort(greater_equal)
+    return result       
+          
+    
