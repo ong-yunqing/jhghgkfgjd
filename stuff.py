113115e4e0eac87c2f0e50dd04021f0be325bd0f
ong-yunqing <yunqing.ong@gmail.com>
2026-07-24 16:59:21 +0800

    finished bubble sort

--- stuff.py
+++ stuff.py
@@ -3,3 +3,15 @@ lst = []
 for i in range(input_size):
     temporary = int(input())
     lst.append(temporary)
+
+def bubble_sort(data):
+    swapped = True
+    passes = len(lst) -1
+    while swapped == True:
+        swapped = False
+        for i in range(passes-1):
+            if data[i] > data[i+1]:
+                data[i], data[i+1] = data[i+1], data[i]
+                swapped = True
+            passes = passes -1
+            
