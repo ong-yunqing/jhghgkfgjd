d8c6d868055c31473b5aded7318d0a52623c08da
Wang Ziyuan <skyler24sg@gmail.com>
2026-07-24 17:35:54 +0800

    Update stuff.py

--- stuff.py
+++ stuff.py
@@ -2,17 +2,17 @@ def quicksort(lst):
     if not lst:
         return []
     equal_lst = []
-    equal = lst[len(lst)//2]
     less_than = []
-    greater_equal = []
+    greater_than = []
+    equal = lst[len(lst)//2]
     for num in lst:
         if num < equal:
             less_than.append(num)
         elif num > equal:
-            greater_equal.append(num)
+            greater_than.append(num)
         else:
             equal_lst.append(num)
-    result = quicksort(less_than) + equal_lst + quicksort(greater_equal)
+    result = quicksort(less_than) + equal_lst + quicksort(greater_than)
     return result       
           
     
