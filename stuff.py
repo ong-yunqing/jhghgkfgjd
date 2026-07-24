8a8fa69127c3a0e9bbd30d3d1cc5ca01dfd76284
shijie-wq <shijiesg@gmail.com>
2026-07-24 17:40:16 +0800

    Add quicksort function to sort lists
    Implement quicksort algorithm for sorting lists.

--- stuff.py
+++ stuff.py
@@ -78,3 +78,21 @@ def insertion_sort(lst):
       pos -= 1
 
     lst[pos] = curr
+
+
+def quicksort(lst):
+    if not lst:
+        return []
+    equal_lst = []
+    less_than = []
+    greater_than = []
+    equal = lst[len(lst)//2]
+    for num in lst:
+        if num < equal:
+            less_than.append(num)
+        elif num > equal:
+            greater_than.append(num)
+        else:
+            equal_lst.append(num)
+    result = quicksort(less_than) + equal_lst + quicksort(greater_than)
+    return result  
