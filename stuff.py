4b8dfeaf7380255c4cd85a65efc762061d666bac
shijie-wq <shijiesg@gmail.com>
2026-07-24 17:45:02 +0800

    Refactor insertion_sort for correct logic
    Fix indentation and variable usage in insertion sort.

--- stuff.py
+++ stuff.py
@@ -69,15 +69,23 @@ def merge_sort(data):
         sorted_l1 = merge_sort(l1)
         sorted_l2 = merge_sort(l2)
         return merge(sorted_l1,sorted_l2)
+        
 def insertion_sort(lst):
-  for i in range(1, len(A)-1):
-    curr = lst[i]
-    pos = i
-    while pos > 0 and lst[pos - 1] >= curr:
-      lst[i] = lst[pos-1]
-      pos -= 1
+    for i in range(1, len(A)-1):
+        curr = lst[i]
+        pos = i
+        while pos > 0 and lst[pos - 1] >= curr:
+          lst[i] = lst[pos-1]
+          pos -= 1
 
     lst[pos] = curr
+
+    return lst
+
+
+    
+
+ 
 
 
 def quicksort(lst):
