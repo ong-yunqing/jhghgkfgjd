174666e991a1bddc06bb7e8cf9a30c7ff938a587
shijie-wq <shijiesg@gmail.com>
2026-07-24 17:05:09 +0800

    Insertion sort

--- stuff.py
+++ stuff.py
@@ -1,1 +1,15 @@
+def insertion_sort(lst):
+  for i in range(1, len(A)-1):
+    curr = lst[i]
+    pos = i
+    while pos > 0 and lst[pos - 1] >= curr:
+      lst[i] = lst[pos-1]
+      pos -= 1
+
+    lst[pos] = curr
+    
+
+insertion_sort([4,2,4,5,10,3])
+
+
 
